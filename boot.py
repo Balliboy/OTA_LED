6b6c597d04206e294203c1d557572ccb0c57cb0e
Balliboy <131509387+Balliboy@users.noreply.github.com>
2023-04-22 12:51:53 +0530

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,11 +1,1 @@
- import machine
- import time
- 
- LED = machine.Pin(13,machine.Pin.OUT)
- 
- for i in range(100):
-     LED.on()
-     time.sleep()
-     LED.off()
-     time.sleep()
-#Trial two
+import machine; machine.Pin(13, machine.Pin.OUT).on()
